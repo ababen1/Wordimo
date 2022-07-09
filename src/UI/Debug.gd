00091944e97d95f66aed31fb70f4f783bbb69924
ben <ben.egg.gov@gmail.com>
2022-07-09 21:21:04 +0300

    removed unused assets

--- src/UI/Debug.gd
+++ src/UI/Debug.gd
@@ -1,11 +1,4 @@
 extends Control
 
-onready var words_funcs = WordsFuncs.new()
-onready var game = $GameBoard
-
 func _ready() -> void:
-	ThemeManger.current_theme = preload("res://assets/Themes/CasualBlue.tres")
-				
-	
-func _on_LineEdit_text_entered(new_text: String) -> void:
-	print(words_funcs.find_word(new_text))
+	pass
