63ef215a491ea06366a4bc9f2400106c05aa1fa7
ben <ben.egg.gov@gmail.com>
2022-07-23 12:52:52 +0300

    small changes

--- src/Global/WordTetrisTheme.gd
+++ src/Global/WordTetrisTheme.gd
@@ -1,19 +1,26 @@
-
 extends Theme
 class_name WordTetrisTheme
 
 const NODE_TYPE_GRID = "GameGrid"
 
-export var primary_color: Color
-export var secondary_color: Color
+export var primary_color: Color setget set_primary_color
+export var secondary_color: Color setget set_secondary_color
 
-func _init() -> void:
-	if not has_stylebox("tile", NODE_TYPE_GRID) and not has_stylebox("tile_alt", NODE_TYPE_GRID):
-		create_tiles_from_colors()
+func set_primary_color(val: Color):
+	primary_color = val
+	if not has_stylebox("tile", NODE_TYPE_GRID):
+		create_tile_from_color("tile", primary_color)
+	emit_changed()
+
+func set_secondary_color(val: Color):
+	secondary_color = val
+	if not has_stylebox("alt_tile", NODE_TYPE_GRID):
+		create_tile_from_color("alt_tile", secondary_color)
+	emit_changed()
 
 func get_board_style() -> BoardStyle:
 	var tile = get_stylebox("tile", NODE_TYPE_GRID) if has_stylebox("tile", NODE_TYPE_GRID) else null
-	var tile_alt = get_stylebox("tile_alt", NODE_TYPE_GRID)
+	var tile_alt = get_stylebox("tile_alt", NODE_TYPE_GRID) if has_stylebox("tile_alt", NODE_TYPE_GRID) else null
 	var board_style: BoardStyle = BoardStyle.new()
 	board_style.primary_color = primary_color
 	board_style.secondary_color = secondary_color
@@ -28,12 +35,9 @@ func get_board_style() -> BoardStyle:
 		board_style.tile_alt = tile_alt
 	return board_style
 
-func create_tiles_from_colors() -> void:
+func create_tile_from_color(tile_prop_name: String, color: Color) -> void:
 	var tile = StyleBoxFlat.new()
-	tile.bg_color = primary_color
-	var tile_alt = StyleBoxFlat.new()
-	tile_alt.bg_color = secondary_color
-	set_stylebox("tile", NODE_TYPE_GRID, tile)
-	set_stylebox("tile_alt", NODE_TYPE_GRID, tile_alt)
+	tile.bg_color = color
+	set_stylebox(tile_prop_name, NODE_TYPE_GRID, tile)
 	
-	
+
